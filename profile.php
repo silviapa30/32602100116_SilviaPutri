572655162e74e252561d38cf764d2518d415c942
Silvia Putri <139944369+silviapa30@users.noreply.github.com>
2023-07-26 17:09:17 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+<section>
+    <h2>My Profile</h2>
+    <p>Hello,Silvia Putri Anggraeni.</p>
+    <p>I like sport.</p>
+</section>
+<?= $this->endSection() ?>
